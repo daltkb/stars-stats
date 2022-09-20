02b6992784201edb70dbca3fd754d1f694c35b8d
daltkb <daltbenn@gmail.com>
2022-09-19 20:34:38 -0500

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import CPGChart from './components/charts/CPGChart';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
+      <Header />
       <CPGChart />
     </div>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <div className="text-center">
+      <h1 className="text-3xl font-bold mt-2">Dallas Stars Charts 2021-2022</h1>
+      <p>
+        All data is from{' '}
+        <a href="https://www.capfriendly.com/" className="underline">
+          CapFriendly
+        </a>
+      </p>
+    </div>
+  );
+}
