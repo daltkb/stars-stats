378007ff68dcf81d6987439d20f2f6a22c5d3115
daltkb <daltbenn@gmail.com>
2022-09-19 20:07:23 -0500

    cost per goal doughnut chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import CPGChart from './components/charts/CPGChart';
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <CPGChart />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/charts/CPGChart.tsx
@@ -0,0 +1,51 @@
+import { Doughnut } from 'react-chartjs-2';
+import { Chart as ChartJS, Tooltip, Legend, ArcElement } from 'chart.js';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.defaults.font.size = 16;
+
+export default function CPGChart() {
+  return (
+    <div style={{ width: '500px', height: '500px', textAlign: 'center' }}>
+      <h1>Cost per goal</h1>
+      <Doughnut data={data} />
+    </div>
+  );
+}
+
+export const data = {
+  labels: [
+    'Jason Robertson',
+    'Roope Hintz',
+    'Joe Pavelski',
+    'Tyler Seguin',
+    'Jamie Benn',
+    'Jacob Peterson',
+  ],
+  datasets: [
+    {
+      label: 'Cost per goal',
+      data: [19390, 85135, 259259, 410416, 527777, 70208],
+      backgroundColor: [
+        'rgba(15, 186, 79, 0.8)',
+        'rgba(130, 130, 130, 0.8)',
+        'rgba(0, 0, 0, 0.8)',
+        'rgba(255, 255, 255, 0.8)',
+        'rgba(0, 255, 32, 0.8)',
+        'rgba(72, 72, 72, 0.8)',
+      ],
+    },
+  ],
+  options: {
+    plugins: {
+      legend: {
+        labels: {
+          color: 'green',
+          font: {
+            size: 20,
+          },
+        },
+      },
+    },
+  },
+};
